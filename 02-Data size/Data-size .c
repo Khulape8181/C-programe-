e3c8476f36cb56ace963604f1d4335ae1c895d89
Khulape8181 <145592872+Khulape8181@users.noreply.github.com>
2023-10-27 19:34:27 +0530

    Create Data-size .c

---
+++ 02-Data size/Data-size .c
@@ -0,0 +1,13 @@
+//write code for data size 
+ //solution:- 
+ #include<stdio.h> 
+ int main () 
+ { 
+     printf("The size of integer dataype is%d\n",sizeof(int)); 
+     printf("The size of charcter datatype is %d\n",sizeof(char)); 
+     printf("The size of double datatype is%d\n",sizeof(double)); 
+     printf("The value of long integer datatype is %d\n",sizeof(long int)); 
+     printf("The value of short  datatype is%d\n",sizeof(short)); 
+     printf("The value of long double is %d\n",sizeof(long double)); 
+     printf("The value of float datatype is %d\n",sizeof(float)); 
+ }
