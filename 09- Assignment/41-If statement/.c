0a6fc580e49b183ce7113c7fa820b1bc94769e35
Khulape8181 <145592872+Khulape8181@users.noreply.github.com>
2024-02-08 13:00:19 +0530

    Create .c

---
+++ 09- Assignment/41-If statement/.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+
+
+int age;
+
+printf("\n\n");
+
+printf("Enter Age : ");
+
+scanf("%d", &age);
+
+if (age >= 18)
+
+{
+
+printf("You Are Eligible For Voting !!!\n\n");
+
+}
+
+return(0);
+}
